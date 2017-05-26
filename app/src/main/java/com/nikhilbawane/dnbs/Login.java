ba1a338d6f509e563a95766f16b58fd3edd8d53d
Nikhil Bawane <likhin7@gmail.com>
2017-05-26 17:08:15 +0530

    Login.class: add butterknife, replace AsyncTask with OkHttp

--- app/src/main/java/com/nikhilbawane/dnbs/Login.java
+++ app/src/main/java/com/nikhilbawane/dnbs/Login.java
@@ -5,34 +5,37 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Login extends AppCompatActivity {
 
-    private EditText mEditUser;
-    private EditText mEditPass;
-    private Button mButton;
-    private TextView mTextResponse;
+    @BindView(R.id.editUser) EditText mEditUser;
+    @BindView(R.id.editPass) EditText mEditPass;
+    @BindView(R.id.button) Button mButton;
+    @BindView(R.id.textResponse) TextView mTextResponse;
+
+    private ProgressDialog progDialog;
 
     private String DNBS_URL;
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
 
 
     @Override
@@ -40,6 +43,8 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         log("onCreate STARTED");
         setTheme(R.style.LoginTheme);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
 
 //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 //            Window w = getWindow(); // in Activity's onCreate() for instance
@@ -48,167 +53,111 @@ public class Login extends AppCompatActivity {
 
         DNBS_URL = getResources().getString(R.string.dnbs_url);
 
+        // Check whether the user is already logged in.
+        // If yes: skip login screen and start DNBS activity.
         if(getSharedPreferences("dnbsPrefs", Context.MODE_PRIVATE).getBoolean("login", false)) {
             Intent intent = new Intent(this, DNBS.class);
             startActivity(intent);
             finish();
         }
 
-        setContentView(R.layout.activity_login);
+        progDialog = new ProgressDialog(this);
+    }
 
-        mEditUser = (EditText) findViewById(R.id.editUser);
-        mEditPass = (EditText) findViewById(R.id.editPass);
-        mButton = (Button) findViewById(R.id.button);
-        mTextResponse = (TextView) findViewById(R.id.textResponse);
+    @OnClick(R.id.button)
+    public void loginButtonClicked() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getBaseContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
 
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ConnectivityManager connMgr = (ConnectivityManager)
-                        getBaseContext().getSystemService(Context.CONNECTIVITY_SERVICE);
-                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        // Check network connectivity
+        if(networkInfo != null && networkInfo.isConnected()) {
+            String user = mEditUser.getText().toString();
+            String pass = mEditPass.getText().toString();
 
-                if(networkInfo != null && networkInfo.isConnected()) {
-                    String user = mEditUser.getText().toString();
-                    String pass = mEditPass.getText().toString();
+            log("loginButtonClicked: user = " + user + " pass = " + pass);
+            if(!user.isEmpty() && !pass.isEmpty()) {
+                String jason = "{"
+                        +   "\"user\":" + "\"" + user + "\"" + ","
+                        +   "\"pass\":" + "\"" + pass + "\""
+                        + "}";
 
-                    log("user = " + user + " pass = " + pass);
-
-                    if(!user.isEmpty() && !pass.isEmpty()) {
-                        String jason = "{"
-                                +   "\"user\":" + "\"" + user + "\"" + ","
-                                +   "\"pass\":" + "\"" + pass + "\""
-                                + "}";
-                        log("jason = " + jason);
-                        new LoginTask(jason).execute();
-                    }
-                    else {
-                        mTextResponse.setText("");
-                    }
-                }
-                else {
-                    mTextResponse.setText("Network connection unavailable");
-                }
-
+                log("loginButtonCLicked: jason = " + jason);
+                login(jason);
             }
-        });
-    }
-
-    private void setTextResponseError() {
-        mTextResponse.setText("Incorrect username or password");
-    }
-
-
-    public class LoginTask extends AsyncTask<Void, Void, Boolean> {
-        private String jason;
-        private ProgressDialog progDialog;
-
-        public LoginTask(String jason) {
-            this.jason = jason;
+            else {
+                mTextResponse.setText("");
+            }
         }
+        else {
+            mTextResponse.setText("Network connection unavailable");
+        }
+    }
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progDialog = new ProgressDialog(Login.this);
+    private void displayProgressDialog(boolean showDialog) {
+        if(showDialog) {
             progDialog.setMessage("Loading...");
             progDialog.setIndeterminate(false);
             progDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
             progDialog.setCancelable(true);
             progDialog.show();
         }
-
-        @Override
-        protected Boolean doInBackground(Void... arg) {
-            try {
-                log("LoginTask STARTED");
-
-                String link = DNBS_URL + "/login.php";
-                URL url = new URL(link);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type", "application/json");
-                conn.setDoOutput(true);
-                conn.connect();
-
-                JSONObject jsonObject = new JSONObject(jason);
-
-                log("LoginTask: JSON: " + jsonObject.toString());
-
-                OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
-                String output = jsonObject.toString();
-                writer.write(output);
-                writer.flush();
-                writer.close();
-
-                log("LoginTask: jsonObject written to output stream.");
-
-                InputStream input = conn.getInputStream();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
-                StringBuilder result = new StringBuilder();
-                String line;
-                log("LoginTask: reader = " + reader.toString());
-
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-                log("LoginTask: result = " + result);
-                log("LoginTask: result = " + result.toString());
-
-                input.close();
-                conn.disconnect();
-
-                getSharedPreferences("dnbsPrefs", MODE_PRIVATE).edit()
-                    .putString("user", result.toString()).apply();
-
-                // Create an object from the json result string
-                JSONObject jsonResult = new JSONObject(result.toString());
-
-                // The server returns an empty if there no users found
-                // Thus we can check if we have a valid user
-                if(!jsonResult.getString("role").equals("")) {
-                    // user found
-                    return true;
-                }
-                else {
-                    // invalid user (or password)
-                    return false;
-                }
-            }
-            catch (JSONException e) {
-                log("LoginTask: JSONException: " + e.getMessage());
-            }
-            catch (IOException e) {
-                log("LoginTask: IOException: " + e.getMessage());
-            }
-            catch(Exception e) {
-                log("LoginTask: Exception: " + e.getMessage());
-            }
-
-            return false;
-        }
-
-        @Override
-        public void onPostExecute(Boolean arg) {
-            super.onPostExecute(arg);
-            log("LoginTask: onPostExecute: arg = " + arg.toString());
-            // The doInBackground passes return value to onPostExecute
-            // Here we get a Boolean value
-            // if the value is true, it means the login was successful
-            //    and thus we can enter the app by calling an Intent to DNBS class.
-            // else we show an error
-
-            if(arg) {
-                getSharedPreferences("dnbsPrefs", MODE_PRIVATE).edit().putBoolean("login", true).apply();
-                Intent intent = new Intent(getBaseContext(), DNBS.class);
-                startActivity(intent);
-                finish();
-            }
-            else {
-                setTextResponseError();
-            }
-
+        else
             progDialog.dismiss();
+    }
+
+    private void setTextResponseError() {
+        mTextResponse.setText("Incorrect username or password");
+    }
+
+    private void login(String jsonText) {
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON, jsonText);
+        Request request = new Request.Builder()
+                .url(DNBS_URL + "/login.php")
+                .post(body)
+                .build();
+        displayProgressDialog(true);
+
+        try {
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    call.cancel();
+                    log("login failure exception: " + e.getMessage());
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    final String result = response.body().string();
+                    log("login response : " + result);
+
+                    Login.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            // The server returns an empty if there no users found
+                            // Thus we can check if we have a valid user
+                            if(result.isEmpty()) {
+                                displayProgressDialog(false);
+                                setTextResponseError();
+                            }
+                            else {
+                                getSharedPreferences("dnbsPrefs", MODE_PRIVATE).edit()
+                                        .putString("user", result)
+                                        .putBoolean("login", true).apply();
+                                Intent intent = new Intent(getBaseContext(), DNBS.class);
+                                displayProgressDialog(false);
+                                startActivity(intent);
+                                finish();
+                            }
+                        }
+                    });
+                }
+            });
+        }
+        catch (Exception e) {
+            log("login exception: " + e.getMessage());
         }
     }
 
